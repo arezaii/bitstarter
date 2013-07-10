42051acec82fcd6dfcd60645437bdfc3890f64eb
Ubuntu <ubuntu@ip-172-31-46-83.us-west-2.compute.internal>
2013-07-10 15:34:03 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var fs = require('fs');
+var buffer = new Buffer(fs.readFile(index.html));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(fs.readFile(index.html));
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
