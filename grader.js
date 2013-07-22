a9730d1a9e2e205deaa069725474f5f3dd0d360e
Ubuntu <ubuntu@ip-172-31-46-83.us-west-2.compute.internal>
2013-07-22 16:57:28 +0000

    customized index.html with my idea

--- grader.js
+++ grader.js
@@ -24,25 +24,19 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('./restler');
+var rest = require('restler');
 var sys = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URLFILE_DEFAULT = "http://google.com";
+var checksfile ;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
-};
-
-var restURLFile = function(URL) {
-    return rest.get(URL).on('complete', function(result) {
-	sys.puts(result);
-    }
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -53,32 +47,53 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(cheeriofile) {
+    $ = cheeriofile;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
+};
+
+var getFileAtURL = function(url) {
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) {
+	    sys.puts('Error: ' + result.message);	    
+	}else {
+	    var checkJson = checkHtmlFile(cheerio.load(result));        
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	}
+    })
 };
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
+
 };
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_file>', 'URL Path', clone(assertFileExists), URLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>', 'URL Path')
+	.parse(process.argv);
+    checksfile = program.checks; 
+    if (program.url){
+	getFileAtURL(program.url);
+    }
+    else if (program.file) {
+	var checkJson =  checkHtmlFile(cheerioHtmlFile(program.file));
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+    
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+
+exports.checkHtmlFile = checkHtmlFile;
 }
--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var fs = require('fs');
 var buffer = new Buffer(fs.readFileSync('index.html'));
-
-var app = express.createServer(express.logger());
+//var htmlfile = "index.html";
+var app = express(); //.createServer(express.logger());
 
 app.get('/', function(request, response) {
     response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
