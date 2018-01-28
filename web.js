a9d6cd6170963dc1c77a865112189b9f17de8a6e
Ahmad Rezaii <ahmad.rezaii@gmail.com>
2018-01-27 19:32:46 -0700

    trying to get it running again

--- web.js
+++ web.js
@@ -2,7 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var buffer = new Buffer(fs.readFileSync('index.html'));
 //var htmlfile = "index.html";
-var app = express() //.createServer(express.logger());
+var app = express() 
+//.createServer(express.logger());
 
 app.get('/', function(request, response) {
     response.send(buffer.toString());
