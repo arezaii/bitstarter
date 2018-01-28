4370667f8127c194ae3d10ce873e3caf1549874f
Ahmad Rezaii <ahmad.rezaii@gmail.com>
2018-01-27 19:36:15 -0700

    trying to get it running again

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var fs = require('fs');
-var buffer = new Buffer(fs.readFileSync('index.html'));
+var express = require('express')
+var fs = require('fs')
+var buffer = new Buffer(fs.readFileSync('index.html'))
 //var htmlfile = "index.html";
 var app = express() 
 //.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(buffer.toString());
+    response.send(buffer.toString())
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening on " + port)
 });
